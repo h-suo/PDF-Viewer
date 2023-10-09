e970ac179c245bd252c32b44c7efaa73c5187c8e
h-suo <phstoo1021@naver.com>
2023-10-09 14:34:49 +0900

    🩹Chore: 스토리보드 삭제 및 코드베이스 세팅

--- PDFViewer/PDFViewer/ViewController.swift
+++ PDFViewer/PDFViewer/ViewController.swift
@@ -2,18 +2,16 @@
 //  ViewController.swift
 //  PDFViewer
 //
-//  Created by 표현수 on 2023/10/09.
+//  Created by Erick on 2023/10/09.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
